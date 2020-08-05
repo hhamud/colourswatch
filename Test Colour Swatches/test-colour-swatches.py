6deeed2acdee00302a684e34d91ac0a1c65e393e
user <hamza.h.hamud@gmail.com>
2020-08-06 00:17:40 +0100

    solved RGB565 to RGB888

--- Test Colour Swatches/test-colour-swatches.py
+++ Test Colour Swatches/test-colour-swatches.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import itertools
+import math
 
 
 
@@ -16,21 +17,42 @@ class IColourSwatchDebug():
         self.BottomLeftColour = BottomLeftColour
         self.bottomRightColour = bottomRightColour
 
-    def RGB565_TO_RGB888(self):
+    def Left_RGB565_TO_RGB888(self):
         #RGB = (R*65536)+(G*256)+B 
         #RGB565 = (R*64*32) + (G *32) + B
-        B = self.topLeftColour - R * 64 * 32 - G * 32
-        G = (self.topLeftColour - B - R * 32 * 64)/32
-        R = (self.topLeftColour - G*32 - B)/(64*32)
+        RL = math.trunc( self.topLeftColour / 2048 )
+        GL = math.trunc( (self.topLeftColour % 2048) / 32 )
+        BL = self.topLeftColour % 32
 
-        gradient_array = []
-        for i in range(0, self.width+1):
-            gradient_value = self.topLeftColour - i * (self.topLeftColour/self.width)
-            gradient_array.append(gradient_value)
-        print(gradient_array)
-            
+        print(RL, GL, BL)
 
-            
+        rL = (RL)*8
+        gL= (GL)*4 
+        bL = (BL)*8
+     
+
+        LRGB888 = [rL,gL,bL]
+
+        print(LRGB888)
+
+        return LRGB888
+
+    def Right_RGB565_TO_RGB888(self):
+        RR = math.trunc( self.topRightColour / 2048 )
+        GR = math.trunc( (self.topRightColour % 2048 ) / 32 )
+        BR = self.topRightColour % 32
+
+        print(RR, GR, BR)
+
+        rR = (RR)*8 
+        gR= (GR)*4 
+        bR = (BR)*8 
+
+        RRGB888 = [rR,gR,bR]
+
+        print(RRGB888)
+
+        return RRGB888
 
 
 
@@ -40,9 +62,16 @@ class IColourSwatchDebug():
         #
         #convert from RGB565 to RGB_default
         #interpolate from the gradient 
+        splitter = self.width
+
+        for i in range(0, splitter + 1):
+            
+
         numpy_array = np.zeros([self.height, self.width, 3], dtype=np.uint8)
-        numpy_array[:,:100] = self.topLeftColour
-        numpy_array[:,100:] = self.topRightColour
+        numpy_array[:,:100] = self.Left_RGB565_TO_RGB888()
+        numpy_array[:,100:] = self.Right_RGB565_TO_RGB888()
+
+
         numpy_image = Image.fromarray(numpy_array)
         numpy_image.save('test.png')
 
@@ -51,5 +80,5 @@ class IColourSwatchDebug():
 
 
 
-c1 = IColourSwatchDebug(6,1,65535, 0)
-c1.RGB565_TO_RGB888()
+c1 = IColourSwatchDebug(200,100,2016, 0)
+c1.createColourSwatch()
